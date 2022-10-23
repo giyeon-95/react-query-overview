46bafd71de497404db8a14577b9c116abb397829
juho <marseille426@naver.com>
2022-10-23 19:05:01 +0900

    테스트 설정

--- lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,18 @@
 import { screen } from '@testing-library/react';
-
+import { renderWithQueryClient } from 'test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
-  // write test here
+//! Treatments 컴포넌트 테스트 (useTreatments 쿼리 테스트)
+test('renders response from query',async () => {
+  renderWithQueryClient(<Treatments/>)
+
+  const treatmentTitles = await screen.findAllByRole('heading',
+  {
+    name: /massage|facial|scrub/i, 
+  });
+  
+  console.log(22, treatmentTitles);
+  
+
+  expect(treatmentTitles).toHaveLength(3);
 });
--- lazy-days/client/src/test-utils/index.tsx
+++ lazy-days/client/src/test-utils/index.tsx
@@ -1,6 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+//! 기본 쿼리 클라이언트 생성
+const generateQueryClient = () =>{
+    return new QueryClient (); 
+}
+
+//! 테스트용 쿼리클라이언트 선언
+export function renderWithQueryClient(
+    ui : ReactElement,
+    client? : QueryClient
+) : RenderResult {
+    const queryClient = client ?? generateQueryClient(); 
+
+    return render(
+        <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>
+    )
+}
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
+
+
 
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = (): React.FC => {
